be77273facb180c0bcec49bf83962cb722707aac
deividthiago2 <deivid.matoso@escola.pr.gov.br>
2026-03-03 09:21:20 -0300

    ok

--- supabaseClient.js
+++ supabaseClient.js
@@ -3,6 +3,6 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 // Cria o client com a URL e a chave fornecidas
 export const supabase = createClient(
-  'url: https://szwqyfhsfmusduoqeway.supabase.co',
+  'https://szwqyfhsfmusduoqeway.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6d3F5ZmhzZm11c2R1b3Fld2F5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIxMTE3NzUsImV4cCI6MjA4NzY4Nzc3NX0.GI0y8n7D3JhCx5ZNplQ5sgw6KjKkkEQnAeKAcD0k9ek'
 )
